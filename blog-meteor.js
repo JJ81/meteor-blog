ed296deb51e8e95f4a096f6421f3ad8241446791
yijaejun <yijaejun81@gmail.com>
2016-03-02 14:18:13 +0900

    Basic settings...

--- blog-meteor.js
+++ blog-meteor.js
@@ -1,23 +1,35 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+    /**
+    * hello's helpers
+    * helper란 무엇인가?
+    *
+    */
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
+
+    // hello's events
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
+} // End Client Code
+
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+    Meteor.startup(function () {
+        // code to run on server at startup
+        console.info('[info] server starting...');
+    });
+
+
+
+} // End Server Code
